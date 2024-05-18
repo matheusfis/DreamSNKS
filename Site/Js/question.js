089c79cdd756a1b0e2164246fb37ddd3ae3aa663
matheusfis <matheus.fischer@sptech.school>
2024-05-18 16:49:30 -0300

    Perguntas do quiz elaboradas

---
+++ Site/Js/question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        questions: "Qual foi o tênis que NÃO foi lançado no ano de 2024?",
+        answer: "C. Air Jordan 1 UNC Toe",
+        options: [
+            "A. Air Jordan 4 Bred Reimagined",
+            "B. Air Max DN",
+            "C. Air Jordan 1 UNC Toe",
+            "D. Dunk SB Rayssa"
+        ]
+    },
+    {
+        numb: 2,
+        questions: "2. Qual tênis Michael B. Jordan usou na final de 1980 em que deu uma virada fantástica?",
+        answer: "A. Air Jordan 1 Retro High OG",
+        options: [
+            "A. Air Jordan 1 Retro High OG",
+            "B. Dunk SB Born x Raised Low One Block At A Time",
+            "C. Air Jordan 11 Retro Concord",
+            "D. Yeezy Boost 700 Wave Runner"
+        ]
+    },
+    {
+        numb: 3,
+        questions: "Qual tênis foi banido da NBA por violar as regras da liga?",
+        answer: "A. Air Jordan 1 High Bred Banned ",
+        options: [
+            "A. Air Jordan 1 High Bred Banned",
+            "B. Nike Kyrie VI AS",
+            "C. Air Jordan 3 White Cement",
+            "D. Lebron 17 Lakers"
+        ]
+    },
+    {
+        numb: 4,
+        questions: "Qual tênis foi criado em homenagem a uma instituição que trata sobre a depressão?",
+        answer: "B. Nike Dunk SB Why So Sad",
+        options: [
+            "A. Air Max 90 Mental Health Awareness",
+            "B. Nike Dunk SB Why So Sad",
+            "C. Adidas Ultraboost Hope",
+            "D. Puma Suede Positive Vibes"
+        ]
+    },
+    {
+        numb: 5,
+        questions: "Qual a cor predominante do tênisNike  Dunk SB Why So Sad?",
+        answer: "",
+        options: [
+            "A. Azul",
+            "B. Vermelho",
+            "C. Amarelo",
+            "D. Verde"
+        ]
+    },
+];
