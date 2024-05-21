946253d1211918e3c4de17c3d38ca01bb13a7f7f
matheusfis <matheus.fischer@sptech.school>
2024-05-21 15:50:30 -0300

    Alterações básicas

--- Site/Js/login.js
+++ Site/Js/login.js
@@ -4,8 +4,36 @@ const loginLink = document.querySelector('.login-link');
 
 registerLink.onclick = () => {
     wrapper.classList.add('active');
+
+    document.getElementById('ipt_nome_log').value = "";
+    document.getElementById('ipt_senha_log').value = "";
 }
 
 loginLink.onclick = () => {
     wrapper.classList.remove('active');
+
+    document.getElementById('ipt_nome_cad').value = "";
+    document.getElementById('ipt_email_cad').value = "";
+    document.getElementById('ipt_senha_cad').value = "";
 }
+
+function redirect() {
+    
+    let nome_cad = document.getElementById('ipt_nome_cad').value;
+    let email_cad = document.getElementById('ipt_email_cad').value;
+    let senha_cad = document.getElementById('ipt_senha_cad').value;
+    let nome_log = document.getElementById('ipt_nome_log').value;
+    let senha_log = document.getElementById('ipt_senha_log').value;
+
+
+if ( (nome_cad != '' && email_cad != '' && senha_cad != '') || (nome_log != '' && senha_log != '' )) {
+    event.preventDefault();
+    window.location.href = "../View/Quiz.html";
+} else {
+    alert('Preencha todos os campos')
+}
+
+
+
+}
+
